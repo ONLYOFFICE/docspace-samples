f454513376203b251dce89afc99f5aef6ebe264e
sshakndr <aleksandr.kondratyev@onlyoffice.com>
2025-03-06 15:43:28 +0700

    improvement of index.js

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT;
-const PORTAL_SRC = process.env.PORTAL_SRC;
+const config = {
+    PORTAL_SRC: process.env.PORTAL_SRC,
+    FILE_ID: process.env.FILE_ID,
+    ROOM_ID: process.env.ROOM_ID,
+    LOGIN: process.env.LOGIN,
+    PASSWORD: process.env.PASSWORD,
+}
 
 nunjucks.configure('examples', {
     autoescape: true,
@@ -14,38 +20,67 @@ nunjucks.configure('examples', {
     express: app
 });
 
+function getSamples(p) {
+    const samples = []
+    const root = fs.readdirSync(path.join(...p))
+    root.forEach(e => {
+        if (e.endsWith('.html')) {
+            samples.push(e)
+        } else {
+            const r = getSamples([...p, e])
+            samples.push({folder: e, files: r})
+        }
+    })
+    return samples
+}
+
+function getSampleLink(e, p = "") {
+    if (typeof e === 'string') {
+        return `
+        <li>
+            <a href="/?sample=${path.join(p, e.slice(0, e.lastIndexOf('.')))}">${e.slice(0, e.lastIndexOf('.'))}</a>
+        </li>
+        `
+    } else {
+        return `
+        <ul>${e.folder}
+            ${e.files.map(
+                f => getSampleLink(f, `${p}${e.folder}/`)
+            ).join('\n')}
+        </ul>
+        `
+    }
+}
+
 app.get('/', (req, res) => {
-    let examples = fs.readdirSync(path.join(__dirname, 'examples'))
-    let main = `
+    if (req.query.sample) {
+        const template = req.query.sample + '.html';
+
+        if (!fs.existsSync(path.join(__dirname, 'examples', template))) {
+            res.redirect('/');
+            return;
+        }
+
+        res.render(template, config);
+        return
+    }
+
+    const samples = getSamples([__dirname, 'examples'])
+    const main = `
         <!DOCTYPE html>
         <html lang="en">
             <head>
                 <meta charset="UTF-8">
-                <title>DocSpace JavaScript SDK Examples</title>
+                <title>DocSpace JavaScript SDK Samples</title>
             </head>
             <body>
-                ${examples.map(
-                    e => `
-                        <li>
-                            <a href="/${e.slice(0,e.lastIndexOf('.'))}">${e.slice(0,e.lastIndexOf('.'))}</a>
-                        </li>
-                    `
+                ${samples.map(
+                    e => getSampleLink(e)
                 ).join('\n')}
             </body>
         </html>
     `
     res.send(main);
-});
-
-app.get('/:filename', (req, res) => {
-    let template = req.params.filename + '.html';
-
-    if (!req.params.filename || !fs.existsSync(path.join(__dirname, 'examples', template))) {
-        res.redirect('/');
-        return;
-    }
-
-    res.render(template, {PORTAL_SRC});
 });
 
 app.listen(PORT, () => {
