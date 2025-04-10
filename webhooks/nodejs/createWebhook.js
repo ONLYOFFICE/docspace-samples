6582931502a869696b402ce4d6554524061e81c8
Andrey Savihin <andrey.savihin@onlyoffice.com>
2025-04-10 10:59:43 +0300

    added createWebhook.js

---
+++ webhooks/nodejs/createWebhook.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+
+require("dotenv").config();
+
+
+// Async function to create the webhook
+// Documentation: https://api.onlyoffice.com/docspace/api-backend/usage-api/create-webhook/
+async function createWebhook(name, uri, secretKey, enabled, ssl, triggers, targetId) {
+    const url = process.env.PORTAL_URL + "/api/2.0/settings/webhook";
+    const apiKey = process.env.API_KEY;
+
+    const data = {
+        name,
+        uri,
+        secretKey,
+        enabled,
+        ssl,
+        triggers,
+        targetId
+    };
+
+    try {
+        const response = await axios.post(
+            url,
+            data,
+            {
+                headers: {
+                    Authorization: `Bearer ${apiKey}`,
+                },
+            }
+        );
+
+        console.log("Webhook created successfully:");
+        console.log(JSON.stringify(response.data));
+
+    } catch (error) {
+        console.log("Error creating webhook:");
+        console.log(JSON.stringify(error));
+    }
+}
+
+// TODO: Replace these values ​​with your own.
+const name = "My Webhook"; // Webhook name
+const uri = "http://localhost:8080/webhook"; // Webhook payload url
+const secretKey = process.env.WEBHOOK_SECRET_KEY;
+const ssl = true; // SSL verification
+const triggers = 0; // All triggers are selected by default
+const targetId= ""; // Target ID
+
+createWebhook(name, uri, secretKey, true, ssl, triggers, targetId);
--- webhooks/nodejs/index.js
+++ webhooks/nodejs/index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const crypto = require("node:crypto");
-const fs = require("node:fs");
-const path = require("node:path");
 
 require("dotenv").config();
 
@@ -10,36 +8,31 @@ const app = express();
 
 app.use(express.json());
 
-const envFilePath = path.join(__dirname, ".env");
-
-function getSecretKey() {
-    const envContent = fs.readFileSync(envFilePath, "utf8");
-    return envContent;
-}
-
 function getSecretHash(secretKey, body) {
     const hasher = crypto.createHmac("sha256", Buffer.from(secretKey, "utf8"));
     hasher.update(Buffer.from(body, "utf8"));
     return "sha256=" + hasher.digest("hex").toUpperCase();
 }
 
+// listening head request for verification of the webhook URL
 app.head("/webhook", (req, res) => {
     res.sendStatus(200);
 })
 
+// listening post request for the webhook
 app.post("/webhook", (req, res) => {
     const sig = req.headers["x-docspace-signature-256"];
-    
+
     if (!sig){
         console.error("Unsigned request!");
         res.sendStatus(401);
         return;
     }
-  
-    const secretKey = getSecretKey();
-    
+
+    const secretKey = process.env.WEBHOOK_SECRET_KEY;
+
     const body = JSON.stringify(req.body);
-    
+
     const hash = getSecretHash(secretKey, body);
 
     if (sig !== hash) {
@@ -53,5 +46,5 @@ app.post("/webhook", (req, res) => {
 });
 
 app.listen(8080, () => {
-  console.log("Server started on port 8080");
+    console.log("Server started on port 8080");
 });
